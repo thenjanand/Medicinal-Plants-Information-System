9f909127cb69fa345fe715b4af58f9feba917d7d
Njanand-xenon <njanand.xenon@gmail.com>
2022-05-13 23:03:47 +0530

    Updates

---
+++ precache-manifest.cbdb428b2b337a8c0f42efdee6597829.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd233ae236ad6507ae9805d2f89c48f5",
+    "url": "/MedicinalPlantsInformationSystem/index.html"
+  },
+  {
+    "revision": "deb47b4cd28b63f52b61",
+    "url": "/MedicinalPlantsInformationSystem/static/css/2.cd131656.chunk.css"
+  },
+  {
+    "revision": "d9d922772f389ac0fd4c",
+    "url": "/MedicinalPlantsInformationSystem/static/css/main.9d6a66ce.chunk.css"
+  },
+  {
+    "revision": "deb47b4cd28b63f52b61",
+    "url": "/MedicinalPlantsInformationSystem/static/js/2.820f2708.chunk.js"
+  },
+  {
+    "revision": "597543d83f96085a134cec2eb4314122",
+    "url": "/MedicinalPlantsInformationSystem/static/js/2.820f2708.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9d922772f389ac0fd4c",
+    "url": "/MedicinalPlantsInformationSystem/static/js/main.d79dc3d0.chunk.js"
+  },
+  {
+    "revision": "74dda3508726cf51dcb6",
+    "url": "/MedicinalPlantsInformationSystem/static/js/runtime-main.e77b4918.js"
+  }
+]);
